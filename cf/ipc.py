50ca5d96ca31cc148031732e7b9bc02191421b6f
albrecht.andi <albrecht.andi@localhost>
2008-11-14 10:04:26 +0000

    Some docstrings.

--- cf/ipc.py
+++ cf/ipc.py
@@ -1,5 +1,30 @@
 # -*- coding: utf-8 -*-
 
+# crunchyfrog - a database schema browser and query tool
+# Copyright (C) 2008 Andi Albrecht <albrecht.andi@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Simple UNIX socket server to handle multiple instances.
+
+A very simple protocol is used:
+
+ - Each request and response is a single line.
+ - A request has the format 'cmd:[args]'
+ - Response is a pickled Python value.
+"""
+
 import cPickle
 import logging
 import os
@@ -13,6 +38,7 @@ from cf import IPC_SOCKET
 
 
 class IPCRequestHandler(SocketServer.BaseRequestHandler):
+    """RequestHandler: Talks to the application instance."""
 
     def __init__(self, request, client_address, server):
         self.app = server.app
@@ -20,6 +46,7 @@ class IPCRequestHandler(SocketServer.BaseRequestHandler):
                                                  server)
 
     def _respond(self, value):
+        """Helper that converts the return value and sends it on the socket."""
         logging.info('Response: %r' % value)
         response = cPickle.dumps(value)
         assert len(response) <= 1024, 'Reponse too large'
@@ -32,6 +59,7 @@ class IPCRequestHandler(SocketServer.BaseRequestHandler):
                 raise
 
     def handle(self):
+        """Handles a request."""
         data = self.request.recv(1024)
         logging.info('Received: %r' % data)
         cmd, args = data.split(':', 1)
@@ -55,6 +83,7 @@ class IPCRequestHandler(SocketServer.BaseRequestHandler):
 
 
 class IPCListener(SocketServer.UnixStreamServer):
+    """Simple server that listens on a UNIX socket."""
 
     def __init__(self, app):
         self.app = app
@@ -82,11 +111,13 @@ class IPCListener(SocketServer.UnixStreamServer):
 
 
 class IPCClient(object):
+    """Client."""
 
     def __init__(self):
         pass
 
     def communicate(self, msg):
+        """Send message to listener and convert the pickled response."""
         sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
         try:
             sock.connect(IPC_SOCKET)
@@ -104,24 +135,28 @@ class IPCClient(object):
         return retval
 
     def get_instances(self):
+        """Ask for active instances."""
         try:
             return self.communicate('get-instances:')
         except socket.error, err:
             return []
 
     def new_instance(self, args=None):
+        """Creates a new instance, optionally with args as files to open."""
         try:
             return self.communicate('new-instance:')
         except socket.error, err:
             return None
 
     def open_uri(self, instance_id, uri):
+        """Opens a URI in an existing instance."""
         try:
             return self.communicate('open-uri:%d:%s' % (instance_id, uri))
         except socket.error, err:
             return False
 
     def ping(self):
+        """Just say hello."""
         try:
             return self.communicate('ping:')
         except socket.error, err:
@@ -129,5 +164,6 @@ class IPCClient(object):
 
 
 def get_client():
+    """Return a client instance."""
     client = IPCClient()
     return client
