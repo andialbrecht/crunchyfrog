e48e314f5c2c3e52b829187dcfc711744bd8c911
Andi Albrecht <albrecht.andi@gmail.com>
2009-05-05 20:58:51 +0200

    Updated SQL Server meta information to new database backend.

--- cf/db/backends/mssql.py
+++ cf/db/backends/mssql.py
@@ -18,6 +18,7 @@
 
 """SQL Server backend."""
 
+from cf.db import objects
 from cf.db.backends import Generic, DEFAULT_OPTIONS
 
 
@@ -47,5 +48,55 @@ class MSSql(Generic):
         ver = ver.splitlines()[0].strip()+" - "+ver.splitlines()[-1].strip()
         return ver
 
+    def _query(self, connection, sql):
+        return connection.execute_raw(sql)
+
+    def initialize(self, meta, connection):
+        schemata = objects.Schemata(meta)
+        meta.set_object(schemata)
+        for item in self._query(connection, INITIAL_SQL):
+            schema = meta.find_exact(cls=objects.Schema,
+                                     name=item['table_catalog'])
+            if schema is None:
+                schema = objects.Schema(meta, name=item['table_catalog'],
+                                        parent=schemata)
+                meta.set_object(schema)
+            if item['table_type'] == 'BASE TABLE':
+                cls = objects.Table
+                coll_cls = objects.Tables
+            elif item['table_type'] == 'VIEW':
+                cls = objects.View
+                coll_cls = objects.Views
+            else:
+                continue
+            parent = meta.find_exact(cls=coll_cls, parent=schema)
+            if parent is None:
+                parent = coll_cls(meta, parent=schema)
+                meta.set_object(parent)
+            obj = meta.find_exact(cls=cls, name=item['table_name'],
+                                  parent=parent)
+            if obj is None:
+                obj = cls(meta, name=item['table_name'],
+                          parent=parent)
+                meta.set_object(obj)
+            col = meta.find_exact(cls=objects.Column, name=item['column_name'],
+                                  parent=obj.columns)
+            if col is None:
+                col = objects.Column(meta, name=item['column_name'],
+                                     parent=obj.columns)
+                meta.set_object(col)
+
 
 DRIVER = MSSql
+
+
+INITIAL_SQL = """SELECT t.table_catalog,
+t.table_name, c.column_name, t.table_type
+FROM information_schema.columns c,
+    information_schema.tables t
+WHERE t.TABLE_NAME = c.TABLE_NAME
+      AND t.table_catalog = c.table_catalog
+ORDER BY c.table_catalog,
+         c.TABLE_NAME,
+         c.ordinal_position
+"""
